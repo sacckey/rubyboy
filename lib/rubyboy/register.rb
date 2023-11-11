16bd9dc28b12fdd1dfd4d77c99c02954f26b708e
sacckey <sackeyad24@gmail.com>
2023-11-11 23:52:23 +0900

    Add Register class

---
+++ lib/rubyboy/register.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module Rubyboy
+  class Register
+    attr_reader :value
+
+    def initialize(name:, value:)
+      @name = name
+      @value = value
+    end
+
+    def value=(v)
+      @value = v & 0xff
+    end
+
+    def increment
+      @value = (@value + 1) & 0xff
+    end
+
+    def decrement
+      @value = (@value - 1) & 0xff
+    end
+  end
+end
