f4cae9c90404dce6c2657e86a372c9539b820d33
sacckey <sackeyad24@gmail.com>
2023-11-14 20:27:44 +0900

    Add Timer class

---
+++ lib/rubyboy/timer.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module Rubyboy
+  class Timer
+    def initialize(interrupt)
+      @div = 0
+      @tima = 0
+      @tma = 0
+      @tac = 0
+      @cycles = 0
+      @interrupt = interrupt
+    end
+
+    def step(cycles)
+      before_cycles = @cycles
+      after_cycles = @cycles + cycles
+      @cycles = after_cycles & 0xffff
+
+      @div += after_cycles / 256 - before_cycles / 256
+      @div &= 0xffff
+
+      if @tac[2] == 1
+        divider = case @tac & 0b11
+                  when 0b00 then 1024
+                  when 0b01 then 16
+                  when 0b10 then 64
+                  when 0b11 then 256
+                  end
+
+        tima_diff = (after_cycles / divider - before_cycles / divider)
+        @tima += tima_diff
+        if @tima >= 256
+          @tima &= 0xff
+          @interrupt.request(0b0000_0100)
+        end
+      end
+    end
+
+    def read_byte(byte)
+      case byte
+      when 0xff04
+        @div >> 8
+      when 0xff05
+        @tima
+      when 0xff06
+        @tma
+      when 0xff07
+        @tac | 0b1111_1000
+      end
+    end
+
+    def write_byte(byte, value)
+      case byte
+      when 0xff04
+        @div = 0
+      when 0xff05
+        @tima = value
+      when 0xff06
+        @tma = value
+      when 0xff07
+        @tac = value & 0b111
+      end
+    end
+  end
+end
