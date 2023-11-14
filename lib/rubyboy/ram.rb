a148dba4f02a9a2deeea9f8ed2a0c31a2aecd35a
sacckey <sackeyad24@gmail.com>
2023-11-14 20:25:37 +0900

    Move hram to ram class

--- lib/rubyboy/ram.rb
+++ lib/rubyboy/ram.rb
@@ -2,12 +2,13 @@
 
 module Rubyboy
   class Ram
-    attr_accessor :eram, :wram1, :wram2
+    attr_accessor :eram, :wram1, :wram2, :hram
 
     def initialize
       @eram = Array.new(0x2000, 0)
       @wram1 = Array.new(0x1000, 0)
       @wram2 = Array.new(0x1000, 0)
+      @hram = Array.new(0x80, 0)
     end
   end
 end
