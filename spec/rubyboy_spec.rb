4b28fc6271e21134a49057eefe032f7957bdf7dc
Masaki Komagata <komagata@gmail.com>
2026-07-24 01:55:54 +0900

    Remove placeholder failing spec

--- spec/rubyboy_spec.rb
+++ spec/rubyboy_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rubyboy do
   it 'has a version number' do
     expect(Rubyboy::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
